a1cbde48afefa13233a5c55896a713ae41b59020
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-23 20:15:17 -0400

    Created Event listeners

---
+++ main.js
@@ -0,0 +1,25 @@
+/*******************
+ * EVENT LISTENERS *
+ *******************/
+
+//numbers
+ const button1 = document.getElementById("one");
+ const button2 = document.getElementById("two");
+ const button3 = document.getElementById("three");
+ const button4 = document.getElementById("four");
+ const button5 = document.getElementById("five");
+ const button6 = document.getElementById("six");
+ const button7 = document.getElementById("seven");
+ const button8 = document.getElementById("eight");
+ const button9 = document.getElementById("nine");
+ const button0 = document.getElementById("zero");
+ const buttonDecimal = document.getElementById("decimal");
+
+
+ //operators
+ const buttonPlus = document.getElementById("plus");
+ const buttonMinus = document.getElementById("minus");
+ const buttonMultiply = document.getElementById("multiply");
+ const buttonDivide = document.getElementById("divide");
+ const buttonEquals = document.getElementById("equals-button");
+ const buttonClear = document.getElementById("clear");
